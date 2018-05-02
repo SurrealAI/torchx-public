a4b7ac09845e377a8d07e4c7b1924d4c86029410
LinxiFan <jimfanspire@gmail.com>
2018-05-01 23:57:18 -0700

    Add all known normalizations

--- torchx/layers/normalization.py
+++ torchx/layers/normalization.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from .base import Layer
+from .container import Lambda
 
 
 class BatchNormNd(Layer):
@@ -15,11 +16,11 @@ class BatchNormNd(Layer):
 
         Args:
             dim: 1d, 2d, or 3d BatchNorm
-            input_shape: required by torchx.Layer
             eps: nn.BatchNorm parameter
             momentum: nn.BatchNorm parameter
             affine: nn.BatchNorm parameter
             track_running_stats: nn.BatchNorm parameter
+            input_shape: see torchx.Layer
         """
         super().__init__(
             input_shape=input_shape,
@@ -36,7 +37,8 @@ class BatchNormNd(Layer):
         """
         C from [N, C, ...] input shape        
         """
-        self.batch_norm = self.NormClass(input_shape[1], **self.init_kwargs)
+        channel = input_shape[1]
+        self.batch_norm = self.NormClass(channel, **self.init_kwargs)
 
     def forward(self, x):
         return self.batch_norm(x)
@@ -73,11 +75,11 @@ class InstanceNormNd(Layer):
         
         Args:
             dim: 1d, 2d, or 3d InstanceNorm
-            input_shape: required by torchx.Layer
             eps: nn.InstanceNorm parameter
             momentum: nn.InstanceNorm parameter
             affine: nn.InstanceNorm parameter
             track_running_stats: nn.InstanceNorm parameter
+            input_shape: see torchx.Layer
 
         Warnings:
             the default args `affine=False` and `track_running_stats=False`
@@ -98,7 +100,8 @@ class InstanceNormNd(Layer):
         """
         C from [N, C, ...] input shape
         """
-        self.instance_norm = self.NormClass(input_shape[1], **self.init_kwargs)
+        channel = input_shape[1]
+        self.instance_norm = self.NormClass(channel, **self.init_kwargs)
 
     def forward(self, x):
         return self.instance_norm(x)
@@ -121,3 +124,94 @@ class InstanceNorm3d(InstanceNormNd):
     def __init__(self, *args, **kwargs):
         super().__init__(3, *args, **kwargs)
 
+
+class LayerNorm(Layer):
+    def __init__(self, num_normalize_dim,
+                 eps=1e-5,
+                 elementwise_affine=True,
+                 *, input_shape=None):
+        """
+        http://pytorch.org/docs/stable/nn.html#layernorm
+
+        Args:
+          num_normalize_dim: the number of dimensions to be normalized over,
+            starting from the last dim in the input tensor.
+            e.g. input tensor size [batch=16, C=10, H=20, W=30]
+            - num_dim=1: normalize over [W=30], affine parameters gamma and beta
+                will have shape [30]
+            - num_dim=2: normalize over [H, W], gamma and beta shape [20, 30]
+            - num_dim=3: normalize over [C, H, W], gamma and beta shape [10, 20, 30]
+          eps: nn.LayerNorm parameter
+          elementwise_affine: nn.LayerNorm parameter
+          input_shape: see torchx.Layer
+        """
+        super().__init__(
+            input_shape=input_shape,
+            eps=eps,
+            elementwise_affine=elementwise_affine
+        )
+        self.num_normalize_dim = num_normalize_dim
+
+    def _build(self, input_shape):
+        """
+        e.g. input tensor size [batch=16, C=10, H=20, W=30]
+        - num_dim=1: normalize over [W=30], affine parameters gamma and beta
+            will have shape [30]
+        - num_dim=2: normalize over [H, W], gamma and beta shape [20, 30]
+        - num_dim=3: normalize over [C, H, W], gamma and beta shape [10, 20, 30]
+        """
+        assert self.num_normalize_dim <= len(input_shape) - 1, \
+            'num_normalize_dim must be at most input dim - 1'
+        normalized_shape = input_shape[-self.num_normalize_dim:]
+        self.layer_norm = nn.LayerNorm(normalized_shape, **self.init_kwargs)
+
+    def forward(self, x):
+        return self.layer_norm(x)
+
+    def get_output_shape(self, input_shape):
+        return input_shape
+
+
+class GroupNorm(Layer):
+    def __init__(self, num_groups,
+                 eps=1e-5,
+                 affine=True,
+                 *, input_shape=None):
+        """
+        Args:
+          num_groups: equivalent to LayerNorm if num_groups == 1
+            equivalent to InstanceNorm if num_groups == num_channels
+          eps: nn.GroupNorm parameter
+          elementwise_affine: nn.GroupNorm parameter
+          input_shape: see torchx.Layer
+        """
+        super().__init__(
+            input_shape=input_shape,
+            eps=eps,
+            affine=affine
+        )
+        self.num_groups = num_groups
+
+    def _build(self, input_shape):
+        """
+        self.num_groups must divide input channel
+        """
+        channel = input_shape[1]
+        assert channel % self.num_groups == 0, \
+            'number of groups {} must divide number of channels {}'.format(
+                self.num_groups, channel)
+        self.group_norm = nn.GroupNorm(
+            self.num_groups, channel, **self.init_kwargs)
+
+    def forward(self, x):
+        return self.group_norm(x)
+
+    def get_output_shape(self, input_shape):
+        return input_shape
+
+
+# ========= Same shape layers with no learnable parameters ===============
+_wrap = Lambda.wrap_same_shape_class
+
+LocalResponseNorm = _wrap(nn.LocalResponseNorm)
+CrossMapLRN2d = _wrap(nn.CrossMapLRN2d)
